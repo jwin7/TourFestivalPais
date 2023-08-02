dbf1d458f2b15e3866e763ab9599aa9b6614e12d
Vegagabriel <139825785+Vegagabriel@users.noreply.github.com>
2023-08-02 19:30:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,126 @@
-import tkinter as tk
-import json
+from customtkinter import *
+from PIL import Image, ImageTk
+from models.evento import Evento
+from views.vista_inicio import Vista_Inicio
+from views.vista_login import Vista_Login
+from views.vista_explorar import Vista_Explorar
+from views.vista_eventos import Vista_Eventos
+from views.vista_detalles import Vista_Detalles
+from views.vista_comentarios import Vista_Comentarios
+from views.vista_mapa import Vista_Mapa
+from views.vista_usuario import Vista_Usuario
+from controllers.controlador_inicio import Controlador_Inicio
+from controllers.controlador_login import Controlador_login 
+from controllers.controlador_explorar import Controlador_Explorar
+from controllers.controlador_eventos import Controlador_Eventos
+from controllers.controlador_detalles import Controlador_Detalles
+from controllers.controlador_mapa import Controlador_Mapa
+from controllers.controlador_usuario import Controlador_usuario
+from models.ubicacion import Ubicacion
 from models.review import Review
 from models.usuario import Usuario
 
-# Crear una ventana de Tkinter
-root = tk.Tk()
-root.title('Tour Festival Pais')
+set_appearance_mode("dark")
 
-# Crear un widget Label para mostrar el nombre del usuario
-nombre_label = tk.Label(root, text='Nombre:')
-nombre_label.pack()
+class App(CTk):
+    def __init__(self, imagenes=[]):
+        super().__init__()
+        #Setup principal
+        self.title("App Tour Musical 0.1")
+        self.geometry("700x500+500+200")
+        self.minsize(600,400)
+        self.maxsize(800,600)
 
-# Crear un widget Entry para permitir al usuario ingresar su nombre
-nombre_entry = tk.Entry(root)
-nombre_entry.pack()
+        #Se cargan los eventos y las ubicaciones
+        self.eventos = Evento.cargar_de_json("data/evento.json")
+        self.ubicaciones = Ubicacion.cargar_de_json("data/ubicacion.json")
+        self.comentarios = Review.cargar_de_json("data/review.json")
+        self.usuarios = Usuario.cargar_de_json("data/usuario.json")
+        self.imagenes = imagenes
 
-# Crear un widget Canvas para mostrar el mapa
-mapa_canvas = tk.Canvas(root, width=400, height=300)
-mapa_canvas.pack()
+        #Inicializar
+        self.inicializar()
+        self.cargar_imagenes()
 
-# Crear un widget Listbox para mostrar la lista de destinos
-destinos_listbox = tk.Listbox(root)
-destinos_listbox.pack()
+        #Run 
+        self.mainloop()
 
-# Iniciar el bucle principal de Tkinter
-root.mainloop()
+    def inicializar(self):
+        #Se cargan los controladores y se les asigna la lista de eventos
+        self.controlador_inicio = Controlador_Inicio(self)
+        self.controlador_login = Controlador_login(self)
+        self.controlador_explorar = Controlador_Explorar(self)
+        self.Controlador_eventos = Controlador_Eventos(self, self.eventos)
+        self.controlador_detalles = None
+        self.controlador_mapa = None
+        self.controlador_usuario = None
+        self.vista_usuario = None
 
-
-
-# Cargar datos de los archivos JSON
-data_reviews = json.load(open('data/review.json'))
-data_usuarios = json.load(open('data/usuario.json'))
-
-# Crear listas de objetos Review y Usuario
-reviews = [Review.from_json(json.dumps(review_data)) for review_data in data_reviews["reviews"]]
-usuarios = [Usuario.from_json(json.dumps(usuario_data)) for usuario_data in data_usuarios["usuarios"]]
-
-# Función para obtener las reseñas y calificaciones de un evento
-def obtener_resenas_y_calificaciones(id_evento):
-    resenas_calificaciones = []
-    for review in reviews:
-        if review.id_evento == id_evento:
-            usuario = next((usuario for usuario in usuarios if usuario.id == review.id_usuario), None)
-            if usuario:
-                resenas_calificaciones.append((usuario.nombre, review.calificacion, review.comentario, review.animo))
-    return resenas_calificaciones
-
-# Ejemplo de uso:
-evento_id = 1
-resenas_calificaciones_evento = obtener_resenas_y_calificaciones(evento_id)
-for nombre_usuario, calificacion, comentario, animo in resenas_calificaciones_evento:
-    print(f"Usuario: {nombre_usuario}")
-    print(f"Calificación: {calificacion}")
-    print(f"Comentario: {comentario}")
-    print(f"Ánimo: {animo}")
-    print("-------------------------------------")
+        #Se muestra la pantalla inicial
+        self.mostrar_login()
 
 
+    #Añade las imagenes a la lista
+    def cargar_imagenes(self):
+        for evento in self.eventos:
+            imagen = ImageTk.PhotoImage(Image.open(f"assets/{evento.imagen}").resize((200, 200)))
+            self.imagenes.append(imagen)
+
+    def cargar_fondo(self, fondo):
+        return CTkImage(Image.open(f"assets/{fondo}"), size=(700, 500))
+
+    #Mostrar vistas
+    def mostrar_inicio(self):
+        self.vista_inicio = Vista_Inicio(self, self.controlador_inicio)
+
+    def mostrar_login(self):
+        self.vista_login = Vista_Login(self, self.controlador_login)
+
+    def mostrar_explorar(self):
+        self.vista_explorar = Vista_Explorar(self, self.controlador_explorar)
+
+    def mostrar_eventos(self):
+        self.vista_eventos = Vista_Eventos(self.vista_explorar, self.Controlador_eventos)
+        self.vista_eventos.agregar_eventos()
+
+    def mostrar_eventos_filtrados(self):
+        self.vista_eventos = Vista_Eventos(self.vista_explorar, self.Controlador_eventos)
+
+    def mostrar_detalles(self):
+        self.vista_detalles = Vista_Detalles(self, self.controlador_detalles)
+
+    def mostrar_comentarios(self):
+        self.vista_comentarios = Vista_Comentarios(self.vista_detalles.detalles_frame, self.controlador_detalles)
+
+    def mostrar_ubicacion(self):
+        self.vista_mapa = Vista_Mapa(self, self.controlador_mapa)
+
+    def mostrar_usuario(self):
+        if self.vista_usuario is None:
+            self.vista_usuario = Vista_Usuario(self, self.controlador_usuario)  # Si es None crea la ventana
+        else:
+            self.vista_usuario.focus()
+
+    def seleccionar_evento(self, id):
+        for ubicacion, evento in zip(self.ubicaciones, self.eventos):
+            if ubicacion.id and evento.id == id:
+               self.ubicacion_seleccionada = ubicacion
+               self.evento_seleccionado = evento
+               self.controlador_detalles = Controlador_Detalles(self, evento)
+               self.controlador_mapa = Controlador_Mapa(self, ubicacion)
+               
+               print(f"ID del evento seleccionado {id}")
+               print(self.eventos[id-1].nombre)
+
+    def seleccionar_usuario(self,id):
+        for usuario in self.usuarios:
+            if usuario.id == id:
+                print(usuario.nombre)
+                self.controlador_usuario = Controlador_usuario(self, usuario)
+                
+
+
+
+
+
+App()
