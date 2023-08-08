d33ab8259cb7c32e6bb53241e3ca53f28fe177bc
Alberto E15 <107888874+alberto-E16@users.noreply.github.com>
2023-08-08 18:46:46 -0300

    Update ubicacion.py

--- models/ubicacion.py
+++ models/ubicacion.py
@@ -1,35 +1,18 @@
-
 import json
 
+
 class Ubicacion:
-    def __init__(self, id: int, nombre: str, direccion: str, latitud: float, longitud: float):
+    def __init__(self, id: int, direccion: str, 
+                 latitud: float, longitud: float ):
+        
         self.id = id
-        self.nombre = nombre  
         self.direccion = direccion
         self.latitud = latitud
         self.longitud = longitud
 
-    def to_json(self):
-        return {"id": self.id, "nombre": self.nombre, "direccion": self.direccion, "latitud": self.latitud, "longitud": self.longitud}
-
-    #@classmethod
-    #def cargar_de_json(cls, archivo_json):
-        with open(archivo_json, "r") as f:
-            data = json.load(f)
-
-        ubicaciones = []
-        for item in data:
-            ubicacion = cls(id=item["id"], nombre=item["nombre"], direccion=item["direccion"], latitud=item["latitud"], longitud=item["longitud"])
-            ubicaciones.append(ubicacion)
-
-        return ubicaciones
     @classmethod
     def cargar_de_json(cls, archivo):
-        with open(archivo, 'r') as f:
+        with open(archivo, "r") as f:
             data = json.load(f)
-            eventos = [cls(id=int(evento["id"]), nombre=evento["nombre"], artista=evento["artista"],
-               genero=evento["genero"], ubicacion=evento["ubicacion"], hora_inicio=evento["hora_inicio"],
-               hora_fin=evento["hora_fin"], descripcion=evento["descripcion"], imagen=evento["imagen"])
-            for evento in data["eventos"]]
-            
-            return eventos
+        return [cls(**ubicacion) for ubicacion in data]
+
