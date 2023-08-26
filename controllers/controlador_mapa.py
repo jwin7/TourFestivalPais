0d71c363a51217d9e0265ec4f49ad8411928b9cf
jwin7 <139569767+jwin7@users.noreply.github.com>
2023-08-26 20:24:44 -0300

    cambios en main

--- controllers/controlador_mapa.py
+++ controllers/controlador_mapa.py
@@ -1,8 +1,40 @@
 from customtkinter import *
+import folium
+import requests
+import polyline
+
 class Controlador_Mapa:
     def __init__(self, app, ubicacion_seleccionada):
         self.app = app
         self.ubicacion_seleccionada = ubicacion_seleccionada
+        self.route = None
+
+    def cargar_marcadores(self, mapa):
+        imagen = self.app.imagenes[self.ubicacion_seleccionada.id-1]
+        marcador = mapa.agregar_marcador(self.ubicacion_seleccionada, imagen)
+        marcador.hide_image(True)
+
+    #Al clickear el marcador genera la imagen del evento seleccionado
+    def seleccionar_ubicacion(self,marcador):
+        if marcador.image_hidden is True:
+            marcador.hide_image(False)
+        else:
+            marcador.hide_image(True)
+        print("Ubicación seleccionada: ", marcador.text)
+
+    def cargar_marcadores(self, mapa):
+        imagen = self.app.imagenes[self.ubicacion_seleccionada.id-1]
+        marcador = mapa.agregar_marcador(self.ubicacion_seleccionada, imagen)
+        marcador.hide_image(True)
+
+    #Al clickear el marcador genera la imagen del evento seleccionado
+    def seleccionar_ubicacion(self,marcador):
+        if marcador.image_hidden is True:
+            marcador.hide_image(False)
+            print("Ubicación seleccionada: ", marcador.text)
+        else:
+            marcador.hide_image(True)
+
 
     def mostrar_seccion_detalles(self):
         self.app.vista_mapa.destroy()
@@ -14,3 +46,69 @@ class Controlador_Mapa:
 
         self.app.mostrar_explorar()
         self.app.mostrar_eventos()
+
+    def quitar_ruta(self):
+        self.app.vista_mapa.destroy()
+        self.app.mostrar_ubicacion()
+
+    #Recoge la latitud y longitud de los destinos elegidos y crea una ruta
+    def planificar_ruta(self, destino_1, destino_2):
+        self.app.vista_mapa.borrar_marcadores()     #Se borran los marcadores creados previamente
+        for evento in self.app.eventos:
+            if evento.nombre == destino_1:
+                lat_a, lon_a = self.app.ubicaciones[evento.id-1].latitud, self.app.ubicaciones[evento.id-1].longitud
+                self.app.vista_mapa.agregar_marcador(self.app.ubicaciones[evento.id-1], self.app.imagenes[evento.id-1])
+                print("Destino 1: ",lat_a, lon_a)
+            if evento.nombre == destino_2:
+                lat_b, lon_b = self.app.ubicaciones[evento.id-1].latitud, self.app.ubicaciones[evento.id-1].longitud
+                self.app.vista_mapa.agregar_marcador(self.app.ubicaciones[evento.id-1], self.app.imagenes[evento.id-1])
+                print("Destino 2: ",lat_b, lon_b)
+        try: 
+            self.draw_map(lat_a, lon_a, lat_b, lon_b)
+            self.app.vista_mapa.crear_ruta(self.route)
+        except: 
+            #En caso de no elegir un segundo destino al crear la ruta saldrá un error
+            print("Se necesitan al menos 2 destinos para trazar una ruta.")
+
+
+    def draw_map(self, lon_a, lat_a, lon_b, lat_b):
+
+        route_data = get_route(lat_a, lon_a, lat_b, lon_b)
+        self.route = route_data['route']
+
+        if not route_data:
+            print("No se pudo obtener los datos de la ruta.")
+            return None
+
+        map_instance = folium.Map(location=[lon_a, lat_a], zoom_start=15)
+
+        folium.PolyLine(locations=route_data['route'], color="blue").add_to(map_instance)
+        folium.Marker(location=[lon_a, lat_a], popup="Origin").add_to(map_instance)
+        folium.Marker(location=[lon_b, lat_b], popup="Destination").add_to(map_instance)
+
+        return map_instance
+
+
+
+def get_route(pickup_lat, pickup_lon, dropoff_lat, dropoff_lon, url="https://router.project-osrm.org/route/v1/driving/"):
+
+    location = f"{pickup_lat},{pickup_lon};{dropoff_lat},{dropoff_lon}"
+    response = requests.get(url + location)
+
+    if response.status_code != 200:
+        print(f"Error en la solicitud con código de estado {response.status_code}")
+        return None
+
+    response_json = response.json()
+    route = polyline.decode(response_json['routes'][0]['geometry'])
+    start_point = [response_json['waypoints'][0]['location'][1], response_json['waypoints'][0]['location'][0]]
+    end_point = [response_json['waypoints'][1]['location'][1], response_json['waypoints'][1]['location'][0]]
+    distance = response_json['routes'][0]['distance']
+
+    return {
+        'route': route,
+        'start_point': start_point,
+        'end_point': end_point,
+        'distance': distance
+    }
+
--- views/vista_mapa.py
+++ views/vista_mapa.py
@@ -1,13 +1,15 @@
 from customtkinter import *
 from tkintermapview import TkinterMapView
+from PIL import Image, ImageTk
 
-principal = "#FF5733"         
-titulo_color = "#F2F2F2"       
-texto_color = "#D4D4D4"        
-subtitulo_color = "#A0A7AC"    
-borde_color = "#C4C4C4"       
-contenedor_color = "#212E36" 
-cuerpo_color = "#192229" 
+principal = "#52A5E0"
+titulo_color = "#EFF3F5"        #Se suele usar para los titulos y el texto en los botones
+texto_color = "#C8CDD0"         #Para los parrafos de texto
+subtitulo_color = "#A0A7AC"     #Para los subtitulos
+borde_color = "#2A3B47"         #Para el borde de los widgets y para el color del hover
+contenedor_color = "#212E36"    #Para el color del frame principal
+cuerpo_color = "#192229"        #Para los frames secundarios
+
 
 class Vista_Mapa(CTkFrame):
     def __init__(self, parent, controlador):
@@ -19,66 +21,152 @@ class Vista_Mapa(CTkFrame):
         self.pack(expand=True, fill="both")
 
         #Grid Layout
-        self.rowconfigure((0,1,3,4,5,6), weight=1, uniform="a")
+        self.rowconfigure((0,1,2,3), weight=1, uniform="a")
+        self.rowconfigure((4), weight=3, uniform="a")
         self.columnconfigure(0, weight=1, uniform="a")
         self.columnconfigure(1, weight=5, uniform="a")
 
         #Widgets
         self.crear_widgets()
         self.posicion_widgets()
-
-        self.agregar_marcador()
+        self.controlador.cargar_marcadores(self)
 
     def crear_widgets(self):
+        #Frame que contendrá la sección de ubicación
+        self.frame_desplegable = CTkScrollableFrame(self, fg_color=contenedor_color, scrollbar_button_color=cuerpo_color, scrollbar_button_hover_color=borde_color)
         #Frame que mostrará la ubicación del evento en el mapa
-        self.mapa_frame = CTkFrame(self)
+        self.mapa_frame = CTkFrame(self.frame_desplegable)
+        #Frames interiores
+        self.frame_interior_1 = CTkFrame(self.frame_desplegable, fg_color=contenedor_color)
+        self.frame_interior_2 = CTkFrame(self.frame_desplegable, fg_color=cuerpo_color)
+        self.frame_interior_3 = CTkFrame(self.frame_desplegable, fg_color=cuerpo_color)
+        #Grid Layouts
+        self.frame_interior_2.rowconfigure((0,1), weight=1, uniform="a")
+        self.frame_interior_2.columnconfigure((0,1,2), weight=1, uniform="a")
+        self.frame_interior_3.rowconfigure((0,1,2), weight=1, uniform="a")
+        self.frame_interior_3.columnconfigure((0,1,2,3), weight=1, uniform="a")
 
         #Mapa
-        self.mapa = TkinterMapView(self.mapa_frame, corner_radius=0)
+        self.mapa = TkinterMapView(self.mapa_frame, corner_radius=0, height=400)
 
         #Botones
         self.boton_volver = CTkButton(self, text="Volver",
+                                      border_width=2,
                                       fg_color=cuerpo_color,
-                                      border_color= contenedor_color,
+                                      border_color= borde_color,
                                       hover_color=borde_color,
                                       text_color= titulo_color,
                                       font=("Open Sans",20),
                                       command=self.controlador.volver)
         self.boton_detalles = CTkButton(self, text="Detalles", 
+                                        border_width=2,
                                         fg_color=cuerpo_color,
-                                        border_color= contenedor_color,
+                                        border_color= borde_color,
                                         hover_color=borde_color,
                                         text_color= titulo_color,
                                         font=("Open Sans",20),
                                         command=self.controlador.mostrar_seccion_detalles)
         self.boton_ubicacion = CTkButton(self, text="Ubicación",
+                                         border_width=2,
                                          fg_color=cuerpo_color,
-                                         border_color= contenedor_color,
+                                         border_color= borde_color,
                                          text_color= titulo_color,
                                          font=("Open Sans",20),
                                          state="disabled")
+        self.boton_planificar_ruta = CTkButton(self.frame_interior_3, text="Crear ruta",
+                                               border_width=2,
+                                               fg_color=cuerpo_color,
+                                               border_color= borde_color,
+                                               text_color= titulo_color,
+                                               font=("Open Sans",20),
+                                               command=lambda: self.controlador.planificar_ruta(self.destino_1_var.get(),
+                                                                                                self.destino_2_var.get()))
+        self.boton_quitar_ruta = CTkButton(self.frame_interior_3, text="Quitar ruta",
+                                           border_width=2,
+                                           fg_color=cuerpo_color,
+                                           border_color= borde_color,
+                                           text_color= titulo_color,
+                                           font=("Open Sans",20),
+                                           command=self.controlador.quitar_ruta)
 
         #Etiquetas
-        self.ubicacion_etiqueta = CTkLabel(self, text="Ubicación en el mapa",
+        self.ubicacion_etiqueta = CTkLabel(self.frame_interior_1, text="Ubicación en el mapa",
                                            text_color= titulo_color,
                                            font=("Roboto", 30, "bold"))
+        self.planificar_etiqueta = CTkLabel(self.frame_interior_2, text="Planificar Ruta",
+                                            text_color=titulo_color, font=("Roboto", 20))
+        self.planificar_descripcion_etiqueta = CTkLabel(self.frame_interior_2, text="Aquí puedes crear una ruta que podrás usar para asistir a multiples eventos!",
+                                                        text_color=texto_color, font=("Open Sans",15))
+        self.destinos_etiqueta = CTkLabel(self.frame_interior_3, text="Primero elige los destinos que quieras incluír en tu ruta.",
+                                          text_color=texto_color, font=("Open Sans",15))
+        self.destino_1_etiqueta = CTkLabel(self.frame_interior_3, text="Destino 1: ",
+                                           text_color=texto_color, font=("Open Sans",15))
+        self.destino_2_etiqueta = CTkLabel(self.frame_interior_3, text="Destino 2: ",
+                                           text_color=texto_color, font=("Open Sans",15))
 
+        #Variable de contról
+        self.destino_1_var = StringVar(value=self.parent.eventos[self.controlador.ubicacion_seleccionada.id-1].nombre) #Usa el nombre del evento seleccionado como valor predeterminado
+        self.destino_2_var = StringVar(value="Destino 2")
+        
+        #Option_Menu
+        self.option_destino_1 = CTkOptionMenu(self.frame_interior_3, values=[(evento.nombre) for evento in self.parent.eventos],
+                                              variable=self.destino_1_var,
+                                                fg_color=cuerpo_color, 
+                                                button_color=contenedor_color,
+                                                button_hover_color=borde_color,
+                                                dropdown_fg_color=contenedor_color,
+                                                dropdown_hover_color=borde_color,
+                                                dropdown_text_color=titulo_color,
+                                                text_color=titulo_color)
+        self.option_destino_2 = CTkOptionMenu(self.frame_interior_3, values=[(evento.nombre) for evento in self.parent.eventos],
+                                              variable=self.destino_2_var,
+                                                fg_color=cuerpo_color, 
+                                                button_color=contenedor_color,
+                                                button_hover_color=borde_color,
+                                                dropdown_fg_color=contenedor_color,
+                                                dropdown_hover_color=borde_color,
+                                                dropdown_text_color=titulo_color,
+                                                text_color=titulo_color)
 
     def posicion_widgets(self):
-        self.mapa_frame.grid(row=1, column=1, rowspan=5, sticky="nsew", padx=5, pady=5)
+        self.frame_desplegable.grid(row=0, column=1, rowspan=5, sticky="nsew", padx=5, pady=5)
+        self.frame_interior_1.pack(expand=True, fill="x", padx=5, pady=5)
+        self.mapa_frame.pack(expand=True, fill="x", padx=5, pady=5)
+        self.frame_interior_2.pack(expand=True, fill="x", padx=5, pady=5)
+        self.frame_interior_3.pack(expand=True, fill="x", padx=5, pady=5)
         
         self.mapa.pack(expand=True, fill="both")
 
-        self.ubicacion_etiqueta.grid(row=0, column=1)
+        self.ubicacion_etiqueta.grid(row=0, column=0, padx=5, pady=5)
+        self.planificar_etiqueta.grid(row=0, column=0, sticky="w", padx=10, pady=5)
+        self.planificar_descripcion_etiqueta.grid(row=1, column=0, columnspan=3, sticky="w", padx=10, pady=5)
+        self.destinos_etiqueta.grid(row=0, column=0, columnspan=4, sticky="w", padx=10, pady=5)
+        self.destino_1_etiqueta.grid(row=1, column=0, sticky="w", padx=10, pady=5)
+        self.destino_2_etiqueta.grid(row=2, column=0, sticky="w", padx=10, pady=5)
 
         self.boton_volver.grid(row=1, column=0, padx=5, pady=5)
         self.boton_detalles.grid(row=2, column=0, padx=5, pady=5)
         self.boton_ubicacion.grid(row=3, column=0, padx=5, pady=5)
+        self.boton_planificar_ruta.grid(row=1, column=3, padx=10, pady=5)
+        self.boton_quitar_ruta.grid(row=2, column=3, padx=10, pady=5)
+
+        self.option_destino_1.grid(row=1, column=1, sticky="w", padx=5, pady=5)
+        self.option_destino_2.grid(row=2, column=1, sticky="w", padx=5, pady=5)
+
 
 
     #Coloca un marcador en la ubicación del evento seleccionado
-    def agregar_marcador(self):
-        ubicacion_seleccionada = self.controlador.ubicacion_seleccionada
-        self.mapa.set_position(ubicacion_seleccionada.latitud, ubicacion_seleccionada.longitud)
-        self.mapa.set_marker(ubicacion_seleccionada.latitud, ubicacion_seleccionada.longitud)
-        print(ubicacion_seleccionada.latitud, ubicacion_seleccionada.longitud)
+    def agregar_marcador(self, ubicacion, imagen):
+        self.mapa.set_position(ubicacion.latitud+0.001, ubicacion.longitud)
+        marcador = self.mapa.set_marker(ubicacion.latitud, ubicacion.longitud, 
+                                        text=ubicacion.direccion, image=imagen,
+                                        command=self.controlador.seleccionar_ubicacion)
+        return marcador
+    
+    def borrar_marcadores(self):
+        self.mapa.delete_all_marker()
+
+    def crear_ruta(self, ruta):
+        self.mapa.delete_all_path() #Se borra la ruta creada antes de crear una nueva
+        self.mapa.set_path(ruta)
+    
